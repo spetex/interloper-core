5d8c421494e069fe0e72fb87a6b7252d267b26d2
Petr Sahula <mail@spetex.cz>
2017-02-25 18:10:31 +0100

    Introduce new coordinate datatype

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,20 @@ pub mod calculations {
     }
 }
 
+pub mod datatypes {
+    use std::fmt;
+
+    impl fmt::Display for Coordinates {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}:{}:{}", self.x, self.y, self.z)
+        }
+    }
+
+    pub struct Coordinates {
+        pub x: i32,
+        pub y: i32,
+        pub z: i32,
+    }
+}
+
 mod tests;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 extern crate spacesim;
+use spacesim::datatypes;
+use spacesim::calculations;
+
 
 fn main() {
+    let here = datatypes::Coordinates{ x:44, y:32, z:678 };
+
     println!("Starting SpaceSim...");
-    println!("Add {}", spacesim::calculations::add(5,5));
+    println!("Add {}", calculations::add(5,5));
+    println!("Spot: {}", here);
 }
