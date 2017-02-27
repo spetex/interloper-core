c1d63ae05bcb307e997fd422604f66dedbb72201
Petr Sahula <mail@spetex.cz>
2017-02-27 22:34:43 +0100

    Refactor get distance function

--- src/lib.rs
+++ src/lib.rs
@@ -18,20 +18,19 @@ pub mod datatypes {
 pub mod calculations {
     use datatypes::Coordinates;
 
+    pub fn get_squared_delta(a: i32, b: i32) -> i32 {
+        // Math helper
+        return (b - a) * (b - a);
     }
 
     pub fn get_distance(point1: Coordinates, point2: Coordinates) -> f32 {
         // Calculates distance between 2 points in the universe based on coordinates
-        let diff_x: i32 = point2.x - point1.x;
-        let diff_y: i32 = point2.y - point1.y;
-        let diff_z: i32 = point2.z - point1.z;
-        let squared_x: i32 = diff_x * diff_x;
-        let squared_y: i32 = diff_y * diff_y;
-        let squared_z: i32 = diff_z * diff_z;
+        let squared_x: i32 = get_squared_delta(point1.x, point2.x);
+        let squared_y: i32 = get_squared_delta(point1.y, point2.y);
+        let squared_z: i32 = get_squared_delta(point1.z, point2.z);
         let sum_int: i32 = squared_x + squared_y + squared_z;
         let sum_float: f32 = sum_int as f32;
-        let res: f32 = sum_float.sqrt();
-        return res;
+        return  sum_float.sqrt();
     }
 }
 
