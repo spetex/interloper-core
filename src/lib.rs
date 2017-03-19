075cf7c7b4c28501ab1c06011918eae9eecbbd7f
Petr Sahula <mail@spetex.cz>
2017-03-19 23:55:43 +0100

    Simplifying models

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,34 @@
 #![doc(html_root_url = "https://spetex.github.io/spacesim/")]
 //! # Spacesim Library
-//! Hobby library for sumulating celestial mechanics
+//! Hobby library for simulating celestial mechanics
 
 pub mod datatypes;
 pub mod calculations;
 
 pub mod objects {
-    use datatypes;
+    use datatypes::Coordinates;
     /// Celestial is a generic object for everything that can reside in the space.
     pub struct Celestial {
         pub id: i32,
-        pub location: datatypes::Coordinates,
+        pub location: Coordinates,
+    }
+
+    impl Celestial {
+        /// Celestial constructor - creates new celestial object
+        pub fn new(id: i32) -> Celestial {
+            Celestial {
+                id: id,
+                location: Coordinates::new_default(),
+            }
+        }
     }
 
     /// The Universe - Every interaction takes place in the relation to the universe.
     pub struct Universe {
-        pub id: i8,
-        pub num_objects: i32,
-        pub celestials: Vec<Celestial>,
+        id: i8,
+        num_objects: i32,
+        epoch: i32,
+        celestials: Vec<Celestial>,
     }
 
     impl Universe {
@@ -26,10 +37,8 @@ pub mod objects {
             Universe {
                 id: id,
                 num_objects: 0,
-                celestials: vec![Celestial {
-                                     id: 0,
-                                     location: datatypes::Coordinates::new_default(),
-                                 }],
+                epoch: 0,
+                celestials: Vec::new(),
             }
         }
         /// Returns id of the universe.
@@ -38,11 +47,8 @@ pub mod objects {
         }
         /// Spawns a celestial in the origin.
         pub fn spawn(&mut self) {
+            self.celestials.push(Celestial::new(self.num_objects));
             self.num_objects = self.num_objects + 1;
-            self.celestials.push(Celestial {
-                id: self.num_objects,
-                location: datatypes::Coordinates::new_default(),
-            });
         }
         /// Returns a count of all celestials in the given universe.
         pub fn get_celestial_count(&self) -> i32 {
@@ -50,13 +56,11 @@ pub mod objects {
         }
         /// Returns reference to the celestial with given id.
         pub fn get_celestial(&self, id: i32) -> &Celestial {
-            let i: usize = id as usize;
-            &self.celestials[i]
+            &self.celestials[id as usize]
         }
         /// Moves celestial by id to a given position.
-        pub fn set_position(&mut self, id: i32, point: datatypes::Coordinates) {
-            let i: usize = id as usize;
-            self.celestials[i].location = point;
+        pub fn set_position(&mut self, id: i32, point: Coordinates) {
+            self.celestials[id as usize].location = point;
         }
     }
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -39,13 +39,14 @@ fn coordinates_creates_itself_on_specific_place() {
 fn universe_exists() {
     use objects;
     let first_universe = objects::Universe::new(0);
-    assert!(first_universe.id == 0);
+    assert!(first_universe.get_id() == 0);
 }
 
 #[test]
 fn universe_spawns_celestial() {
     use objects;
     let mut first_universe = objects::Universe::new(0);
+    first_universe.spawn();
     first_universe.spawn();
     let celestial_one = first_universe.get_celestial(0);
     let celestial_two = first_universe.get_celestial(1);
@@ -59,9 +60,12 @@ fn celestial_can_move_in_universe() {
     use objects;
     use datatypes;
     let mut first_universe = objects::Universe::new(0);
+    first_universe.spawn();
     first_universe.set_position(0, datatypes::Coordinates::new(76, 43, 444));
     let celestial_one = first_universe.get_celestial(0);
     assert!(celestial_one.location.x == 76);
+    assert!(celestial_one.location.y == 43);
+    assert!(celestial_one.location.z == 444);
 }
 
 #[test]
